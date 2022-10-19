2e8d47cd6093e1b59d1d3d54c1bd8cb883d07a43
Paul Applegate <paul@theapplegates.com>
2022-10-19 13:33:18 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import image from "@astrojs/image";
+import picture from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
+});
